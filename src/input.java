181d1d51d361632e4e5d357a395eaf987ae3a053
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-03-26 23:48:11 +0530

    give input

--- src/input.java
+++ src/input.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class input {
+    public static void main (String [] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("please enter your input : ");
+        int rollno = input.nextInt();
+        System.out.println( "your roll number is : " + rollno);
+    }
 }
