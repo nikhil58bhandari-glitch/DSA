1d22215b2e2dd103bc9502ead594e4567ed9be2e
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-03-27 01:01:57 +0530

    if and while loop

---
+++ src/loops.java
@@ -0,0 +1,13 @@
+public class loops {
+    public static void main(String [] arges){
+//        int a = 1;
+//        if (a ==1){
+//            System.out.println("hello wolrd");
+
+        int count = 1;
+        while (count != 5){
+            System.out.println(count);
+            count++;
+        }
+    }
+}
