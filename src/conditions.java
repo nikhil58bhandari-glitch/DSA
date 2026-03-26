3e0b84c1a8f89e747cf63a843f9dc10baab13b5f
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-03-27 01:22:41 +0530

    if-else conditions

---
+++ src/conditions.java
@@ -0,0 +1,35 @@
+public class conditions {
+    public static void main(String[] args) {
+
+        /*
+        syntax of if statements:
+        if(booleans expression T or F) {
+        // body
+        {
+        else{
+        // do this
+        }
+
+         */
+        int salary = 25000;
+//        if (salary > 10000){
+//            salary = salary + 2000;
+//        }
+//        else{
+//            salary = salary + 1000;
+//        }
+
+        // multiple if-else
+
+        if(salary > 20000) {
+            salary += 3000;
+        }
+        else if (salary > 10000){
+            salary += 2000;
+        }
+        else{
+            salary =+ 1000;
+        }
+        System.out.println(salary);
+    }
+}
