0e9a10ed957ab6a99048f98411c3e8174a7089f1
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-03-27 00:03:02 +0530

    input sring

--- src/input.java
+++ src/input.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class input {
     public static void main (String [] args){
         Scanner input = new Scanner(System.in);
-        System.out.print("please enter your input : ");
-        int rollno = input.nextInt();
-        System.out.println( "your roll number is : " + rollno);
+//        System.out.print("please enter your input : ");
+//        int rollno = input.nextInt();
+//        System.out.println( "your roll number is : " + rollno);
+
+  String name = input.next();
+        System.out.println(name);
     }
 }
