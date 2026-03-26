b37eaa1d33db1082380fca3eaf63dca487ffebb8
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-03-27 01:09:23 +0530

    if, while, for loops

--- src/loops.java
+++ src/loops.java
@@ -1,13 +1,21 @@
 public class loops {
     public static void main(String [] arges){
 //        int a = 1;
+        // if loop
+
 //        if (a ==1){
 //            System.out.println("hello wolrd");
 
-        int count = 1;
-        while (count != 5){
+//     while loop
+//        int count = 1;
+//        while (count != 5){
+//            System.out.println(count);
+//            count++;
+//        }
+
+        // for loop
+        for (int count = 1; count != 5; count++){
             System.out.println(count);
-            count++;
         }
     }
 }
