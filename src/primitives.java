df651416cb69b944cba0ed327d6cb94ef03f78be
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-03-26 23:56:53 +0530

    primitives datatypes

--- src/primitives.java
+++ src/primitives.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class primitives {
+    public static void main (String[] args){
+
+
+
+        int rollno = 64;
+        String name = "Nikhil Bhandari";
+        char letter = 'b';
+        float marks = 76.8f;
+        double largenumber = 2343542532.5332;
+        long longinteger = 3745983957025859520L;
+        boolean check = false;
+
+        System.out.println(rollno);
+        System.out.println(name);
+        System.out.println(letter);
+        System.out.println(marks);
+        System.out.println(largenumber);
+        System.out.println(longinteger);
+        System.out.println(check);
+
+
+    }
 }
