edf8d48435d516b55e32b1a70836ee3239ad0088
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-03-28 00:29:29 +0530

    funtion using String

--- src/functions.java
+++ src/functions.java
@@ -8,18 +8,24 @@ import java.util.Scanner;
 
 public class functions {
     public static void main(String[] args) {
-        sum();
-    }
-//        // Q1-: take input of 2 numbers and print the sum
-//        Scanner in = new Scanner(System.in);
-//        System.out.print("enter 1st number-: ");
-//        int num1 = in.nextInt();
-//        System.out.print("enter 2nd number-: ");
-//        int num2 = in.nextInt();
-//        int sum = num1 + num2;
-//
-//        System.out.println("sum of number - " + sum);
+       // sum();
 
+   /*    String message = greet();
+        System.out.println(message);    */
+    }
+    /*
+        // Q1-: take input of 2 numbers and print the sum
+        Scanner in = new Scanner(System.in);
+        System.out.print("enter 1st number-: ");
+        int num1 = in.nextInt();
+        System.out.print("enter 2nd number-: ");
+        int num2 = in.nextInt();
+        int sum = num1 + num2;
+
+        System.out.println("sum of number - " + sum);
+*/
+    /*
+      return the value
     static void sum(){
             Scanner in = new Scanner(System.in);
             System.out.print("Enter first number - ");
@@ -29,6 +35,10 @@ public class functions {
 
             int sum =  num1 + num2;
         System.out.println("sum of the numvber - " + sum);
-        }
+*/
 
+       static String greet() {
+           String greeting = "how r you ";
+           return greeting;
+       }
 }
