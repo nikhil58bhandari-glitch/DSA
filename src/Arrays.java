eeddb559cf8ef326d82e1f3344ce7bb17f032027
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-03-30 02:36:40 +0530

    Arrays

---
+++ src/Arrays.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+     /* Syntex-:
+     datatype[] variable_name = new datatype[size];
+     int [] rollnum = new int[5];
+              or
+     int [] rollnum = new int {2, 3, 4, 5, 6, 7};
+     */
+
+public class Arrays {
+    public static void main(String[] args) {
+       // int[] arr; // declaration of array. rollnum is getting defined in the stack
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+      //  arr = new int[5];// initialisation: actually here object is being created in the memory (heap)
+        arr[0] = 34;
+        arr[1] = 43;
+        arr[2] = 54;
+        arr[3] = 45;
+        arr[4] = 55;
+
+       // System.out.println(arr[0]);
+        //  String[] arr = new String[4];
+        //   System.out.println(arr[0]); // null
+
+
+        // input using for loops-
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = in.nextInt();
+        }
+        // output
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        // for-each loop
+        for (int j : arr) { // for every element in array, print the element
+            System.out.print(arr[j] + " "); // here j represent element of the array
+        }
+        }
+    }
+
