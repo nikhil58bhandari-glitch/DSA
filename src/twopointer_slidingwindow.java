b746b9c993986d03aac2a57677ef2af9d9f91552
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-13 01:49:52 +0530

    TwoPointer and Sliding Window

--- src/twopointer_slidingwindow.java
+++ src/twopointer_slidingwindow.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-// Q1-: Most Consecutive ones-
+ /* // Q1-: Most Consecutive ones-
 // Longest subarray with at most k zeros:
 
 class Consecutive_ones {
@@ -47,6 +47,45 @@ class Consecutive_ones {
 
         int result = obj.longestOnes(nums, k);
         System.out.println("Max consecutive ones: " + result);
+    }
+}
+*/
+
+// Q2-:  Longest Substring Without Repeating Characters-
+
+class Substring {
+    public int longestNonRepeatingSubstring(String s) {
+
+      int[] hash = new int[256];
+      Arrays.fill(hash, -1);
+
+        int n = s.length();
+        int l = 0, r = 0;
+        int maxlength = 0;
+
+        while(r < n ){
+            char ch = s.charAt(r);
+
+            if(hash[ch] != -1){
+
+                if(hash[ch] >= l){
+                    l = hash[ch] + 1;
+                }
+            }
+           int length = r - l + 1;
+            maxlength = Math.max(length,maxlength);
+
+            hash[ch] = r;
+            r++;
+        }
+        return maxlength;
+    }
+
+    public static void main(String[] args) {
+        Substring obj = new Substring();
+
+        String str = "aaabbbccc";
+        System.out.println(obj.longestNonRepeatingSubstring(str));
     }
 }
 
