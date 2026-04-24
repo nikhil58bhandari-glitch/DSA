a18ad1260a8cc8f9e453b409727ae062518b95b1
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-04-25 00:15:26 +0530

    Linear Search

--- src/linear_search.java
+++ src/linear_search.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
- public class linear_search {
+/*
+  class linear_search {
      public static void main(String[] args) {
          int[] arr = {12, 33, 31, 42, 54, 64, 74, 22};
          int target = 74;
@@ -17,13 +18,55 @@ import java.util.*;
              return -1;
          }
          // run the for loop
-         for (int i = 0; i < arr.length; i++) {
-             // check the element at every index if it is = target
-             if (arr[i] == target) {
-                 return i;
+//         for (int i = 0; i < arr.length; i++) {
+//             // check the element at every index if it is = target
+//             if (arr[i] == target) {
+//                 return i;
+
+         // for each loop
+         for (int num : arr){
+             if (num == target){
+                 return num;
 
              }
          }
+         // this line will execute if none of the return statements above have executed
+         // hence the target not found
          return -1;
      }
  }
+ */
+
+//  Q-: Search in String -
+
+class searchInString {
+    public static void main(String[] args) {
+
+        String name = "nikhil";
+        char target = 'k';
+        System.out.println(Search(name, target));
+    }
+
+    static boolean Search(String str, char target) {
+
+        if (str.length() == 0) {
+            return false;
+        }
+
+//        for (int i = 0; i < str.length(); i++) {
+//            if (str.charAt(i) == target) {
+//                return true;
+
+        for(char ch : str.toCharArray()){
+            if(ch == target){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
+
+
