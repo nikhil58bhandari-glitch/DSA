8737c99951cb2d82fa0fc6a5a12546857950e3e7
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-05-01 00:18:08 +0530

    Binary Search Problems

--- src/binary_Search_prob.java
+++ src/binary_Search_prob.java
@@ -35,6 +35,7 @@ import java.util.*;
  */
 
 // Q2-> floor of a Number-:
+/*
 // floor -: greatest number less than equal to target
 
  class floor{
@@ -64,5 +65,85 @@ import java.util.*;
         return end;
     }
 }
+ */
+
+//  Q3->  Smallest Letter -: LEETCODE
+/*
+class smallestlett{
+    public static void main(String[] args) {
+
+
+    }
+    public char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(target < letters[mid]){
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
+    }
+}
+*/
+
+// Q4-> First and Last Position in Sorted Array -: LEETCODE
+/*
+class firstandlastPosition {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+
+        // check for first occurrence if target first-
+
+        int start = search(nums,target,true);
+        int end  = search(nums,target,false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+
+    // this function just returns the index value of target-
+    int search(int[] nums, int target, boolean findStartIndex) {
+
+        int ans =-1;
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                // potential ans found
+                ans = mid;
+                if (findStartIndex) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
+}
+ */
+
+
+
 
 
