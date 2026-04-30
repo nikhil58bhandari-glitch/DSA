1634b8e0e098498507f21d1457a186c29b6cd1ef
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-05-01 01:09:54 +0530

    Binary Search Problems

--- src/binary_Search_prob.java
+++ src/binary_Search_prob.java
@@ -143,6 +143,56 @@ class firstandlastPosition {
 }
  */
 
+// Q5-> Position of an Element in Infinite Sorted Array-:
+class infiniteElement{
+    public static void main(String[] args) {
+
+        int arr [] = {3, 5, 7, 9, 10, 90, 100, 130, 140, 160, 170};
+        int target = 10;
+        System.out.println(ans(arr,target));
+    }
+    static  int ans(int[] arr, int target) {
+        // first find the range
+        // first start with a box of size 2
+        int start = 0;
+        int end = 1;
+
+        // condition for the target to lie in the range
+        while (target > arr[end]) {
+            int temp = end + 1; // this is my new start
+
+            // double the box value
+            // end = previous end + sizeofbox *2
+            end = end + (end - start + 1) * 2;
+            start = temp;
+        }
+        return searchRange(arr,target, start, end);
+    }
+
+    static int searchRange(int[] arr, int target, int start, int end){
+
+        while(start <= end){
+
+            int mid = start  + (end - start) / 2;
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else{
+                // ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+
 
 
 
