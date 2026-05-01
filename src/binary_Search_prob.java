ddb488d62a70477b00334ed0888f176073bc5fba
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-05-01 23:46:56 +0530

    Binary Search Problems

--- src/binary_Search_prob.java
+++ src/binary_Search_prob.java
@@ -144,6 +144,7 @@ class firstandlastPosition {
  */
 
 // Q5-> Position of an Element in Infinite Sorted Array-:
+/*
 class infiniteElement{
     public static void main(String[] args) {
 
@@ -189,7 +190,41 @@ class infiniteElement{
         return -1;
     }
 }
+ */
 
+
+// Q6-> Peak Index in Mountain Array->
+
+class Mountain{
+    public static void main(String[] args) {
+
+    }
+    public int peakIndexInMountainArray(int[] arr){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end){
+         int mid = start + (end - start) / 2;
+
+         if(arr[mid] > arr[mid + 1]){
+             // you are in dec part of array
+             // this may be the ans, but look at left
+             // this is why end != mid - 1
+             end = mid;
+         } else{
+             // you are in asc part of array
+             start = mid + 1; // because we know that mid+1 element > mid element
+         }
+        }
+        // in the end, start == end and pointing to the largest number becouse of the 2 checks above
+        // start and end are always trying to find max element in the above 2 checks
+        // hence, when they are pointing to just one  element, that os the max one because that is what the checks says
+        // more elaboration : at every point of the start and end, they have the  best possible answer till that time.
+        // and if we are saying that only one itme is remining, hence cuz of above line that is the best possible ans.
+
+        return start ; // or return end as both are =.
+    }
+}
 
 
 
