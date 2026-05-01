0550f264bea7140b5c3e2dd6be9338a097806c54
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-05-02 00:39:35 +0530

    Binary Search Problems

--- src/binary_Search_prob.java
+++ src/binary_Search_prob.java
@@ -194,7 +194,7 @@ class infiniteElement{
 
 
 // Q6-> Peak Index in Mountain Array->
-
+/*
 class Mountain{
     public static void main(String[] args) {
 
@@ -225,6 +225,78 @@ class Mountain{
         return start ; // or return end as both are =.
     }
 }
+ */
+
+// Q7-:  Find in Mountain Array -:
+
+class Mountain {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 3, 5, 7, 6, 4, 2};
+
+        Mountain obj = new Mountain();
+        System.out.println(obj.search(arr, 6));
+    }
+
+    int search(int[] arr, int target) {
+
+        int peak = peakIndexInMountainArray(arr);
+
+        int firstTry = orderAgnosticBS(arr, target, 0, peak);
+        if(firstTry != -1){
+            return firstTry;
+        }
+        return orderAgnosticBS(arr, target,peak + 1, arr.length -1);
+    }
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+
+ int orderAgnosticBS(int [] arr, int target, int start, int end) {
+
+    boolean isAsc = arr[start] < arr[end];
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+
+        if(isAsc){
+            if(target < arr[mid]){
+                end = mid - 1;
+            } else{
+                start = mid + 1;
+            }
+        } else {
+            if(target < arr[mid]){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+}
+    return -1;
+}
+}
+
+
+
 
 
 
