1e1bbd0ccb6946ddf8011092d7a3d662d0cc33cb
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-05-08 01:32:15 +0530

    Array Question

--- src/arrayQuestion.java
+++ src/arrayQuestion.java
@@ -94,8 +94,8 @@ class candies {
 }
  */
 
-// Q7-> 1512-: Number of Good pairs->
-
+// Q6-> 1512-: Number of Good pairs->
+/*
 class goodPairs{
     public int numIdenticalPairs(int[] nums){
         int count = 0;
@@ -110,4 +110,28 @@ class goodPairs{
        return count;
     }
 }
+ */
+
+// Q7-> 1365-: How Many Number are smaller than the current number
+
+class samllerNum{
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+
+        int n = nums.length;
+        int [] ans = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int count = 0;
+            for(int j = 0; j < n; j++){
+                if(j != i && nums[j] > nums[i]){
+                    count++;
+                }
+            }
+            ans[i] = count;
+        }
+        return ans;
+    }
+}
+
+
 
