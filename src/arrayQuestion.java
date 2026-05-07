19e530a98e14b23aaebc93dac8aa29fa3f1a2e0c
nikhil bhandari <nikhil58bhandari@gmail.com>
2026-05-07 11:05:04 +0530

    Array Questions

--- src/arrayQuestion.java
+++ src/arrayQuestion.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 // Q1-> 1920- : Build Array from Permutation-
-
+/*
  class permutation {
      public int[] buildArray(int[] nums) {
          // create new array ans
@@ -14,4 +14,19 @@ import java.util.*;
          return ans;
      }
  }
+ */
+
+ // Q2-> 1929 -: Concatenation of Array-
+
+class concationation{
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int [] ans = new int[2 * n];
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
 
